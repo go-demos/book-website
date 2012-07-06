a33e1edb403297fd4a1580f3f7ac54cae1d9e290
Pavan Sudarshan <itspanzi@gmail.com>
2012-07-07 00:26:01 +0530

    Stored shopping cart info in the session.

--- src/com/thoughtworks/go/website/controllers/BooksController.java
+++ src/com/thoughtworks/go/website/controllers/BooksController.java
@@ -9,8 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,9 +37,28 @@ public class BooksController {
 
     @RequestMapping(value = "/books/buy", method = RequestMethod.POST)
     public ModelAndView buyBook(@RequestParam("isbn") String isbn,
-                                @RequestParam("name") String name) {
-        Map model = new HashMap();
-        model.put("name", name);
-        return new ModelAndView(new RedirectView("/payments/index", true), model);
+                                @RequestParam("name") String name,
+                                HttpServletRequest request) {
+        HttpSession session = request.getSession(true);
+        session.setAttribute("current_book", new BookCookie(name, isbn));
+        return new ModelAndView(new RedirectView("/payments/index", true), new HashMap());
+    }
+
+    public static class BookCookie implements Serializable {
+        String name;
+        String isbn;
+
+        public BookCookie(String name, String isbn) {
+            this.name = name;
+            this.isbn = isbn;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getIsbn() {
+            return isbn;
+        }
     }
 }
--- src/com/thoughtworks/go/website/controllers/PaymentsController.java
+++ src/com/thoughtworks/go/website/controllers/PaymentsController.java
@@ -5,8 +5,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,10 +16,26 @@ import java.util.Map;
 public class PaymentsController {
 
     @RequestMapping(value = "/payments/index", method = RequestMethod.GET)
-    public ModelAndView paymentIndex(@RequestParam("name") String name, HttpServletRequest request) {
+    public ModelAndView paymentIndex(HttpServletRequest request) {
         Map model = new HashMap();
-        model.put("name", name);
+        model.put("book", request.getSession().getAttribute("current_book"));
         return new ModelAndView("payments/index", model);
+    }
+
+    @RequestMapping(value = "/payments/success", method = RequestMethod.GET)
+    public ModelAndView paymentSuccessful(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        Map model = new HashMap();
+        model.put("book", session.getAttribute("current_book"));
+        return new ModelAndView("payments/success", model);
+    }
+
+    @RequestMapping(value = "/payments/perform", method = RequestMethod.POST)
+    public ModelAndView performPayment(@RequestParam("number") String number,
+                                     @RequestParam("expiry") String expiry,
+                                     @RequestParam("cvv") String cvv) {
+        Map model = new HashMap();
+        return new ModelAndView(new RedirectView("/payments/success", true), model);
     }
 
 }
