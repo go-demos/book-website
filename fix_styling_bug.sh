72790cff81f10cde20c31cffb35f04ff58725851
Pavan Sudarshan <itspanzi@gmail.com>
2012-08-16 00:12:28 +0530

    Added a couple of scripts to introduce styling issues.

---
+++ fix_styling_bug.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sed -i 's/padding-top: 5px;/padding-top: 40px;/g' web/stylesheets/main.css
---
+++ introduce_styling_bug.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sed -i 's/padding-top: 40px;/padding-top: 5px;/g' web/stylesheets/main.css
